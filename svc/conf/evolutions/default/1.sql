f1b06a852281c24c8eb726d75a862ac522d2734e
Casburn <robert.casburn@atos.net>
2016-01-19 13:34:27 +0000

    [feature: 99594, added asset table, id asset_owner asset_type additional_information]

--- svc/conf/evolutions/default/1.sql
+++ svc/conf/evolutions/default/1.sql
@@ -18,7 +18,16 @@ CREATE TABLE item (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE asset (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    asset_owner varchar(225) NOT NULL,
+    asset_type varchar(225) NOT NULL,
+    additional_information varchar(225) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 # --- !Downs
 
 DROP TABLE project;
 DROP TABLE item;
+DROP TABLE asset;
